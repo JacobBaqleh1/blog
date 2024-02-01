88a8141663b891bd2499ed5e090610b56982846f
hot funkymess <baqlehjacob1@gmail.com>
2024-01-31 22:30:51 -0800

    first commit

---
+++ app/models/post.server.ts
@@ -0,0 +1,12 @@
+import { prisma } from "~/db.server";
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } });
+  }
+  export async function createPost(post) {
+    return prisma.post.create({ data: post });
+  }
